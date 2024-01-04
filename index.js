6e597f132025559f706a4f77fba8104b13ccd53c
Fatinafqh <fatinafiqahzainuddin@gmail.com>
2024-01-04 15:18:33 +0800

    update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const options = {
     definition: {
       openapi: '3.0.0',
       info: {
-        title: 'WJ BENR3433 INFORMATION SECURITY assignment',
+        title: ' BENR3433 INFORMATION SECURITY assignment',
         version: '1.0.0',
       },
       tags:[
