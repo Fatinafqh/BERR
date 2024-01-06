6b53a0a1eef8aa9bcace90a2d9ce79394d9235a7
Fatinafqh <fatinafiqahzainuddin@gmail.com>
2024-01-06 14:39:06 +0800

    resend

--- mongodb_models/user_schema .js
+++ mongodb_models/user_schema .js
@@ -1,31 +1,13 @@
-const mongoose = require('mongoose')
+// user_schema.js
+const mongoose = require('mongoose');
 
-const userschema = mongoose.Schema(
-    {
-        username:{
-            type: String,
-            required:true
-        },
-        password:{
-            type: String,
-            required: true
-        },
-        name:{
-            type: String,
-            required: true
-        },
-        role:{
-            type: String,
-            required: true
-        },
-        visitor_id:{
-            type: mongoose.Schema.Types.ObjectId,ref:'Visitor'
-        },
-        login_status:{
-            type: Boolean
-        }
-    },
-    { versionKey: false }
-)
-const User = mongoose.model('User', userschema);
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    role: { type: String, default: 'user' },
+    login_status: { type: Boolean, default: false }
+});
+
+const User = mongoose.model('User', userSchema);
 module.exports = User;
--- mongodb_models/visitor_schema .js
+++ mongodb_models/visitor_schema .js
@@ -1,32 +1,13 @@
-const mongoose = require('mongoose')
+// visitor_schema.js
+const mongoose = require('mongoose');
 
-const visitorschema = mongoose.Schema(
-    {
-        full_name:{
-            type: String,
-            required: true
-        },
-        phone_number:{
-            type: String,
-            required: true
-        },
-        email:{
-            type: String,
-            required: true
-        },
-        license_number:{
-            type: String,
-            required: true
-        },
-        user_id:{
-            type: mongoose.Schema.Types.ObjectId, ref:'User'
-        },
-        visitor_pass_id:[{
-            type: mongoose.Schema.Types.ObjectId, ref:'V_pass'
-        }]
-    },
-    { versionKey: false }
-)
+const visitorSchema = new mongoose.Schema({
+    full_name: { type: String, required: true },
+    phone_number: { type: String },
+    email: { type: String },
+    license_number: { type: String, required: true },
+    user_id: { type: mongoose.Schema.Types.ObjectId, required: true } // Assuming a reference to the logged-in user
+});
 
-const Visitor = mongoose.model('Visitor', visitorschema);
+const Visitor = mongoose.model('Visitor', visitorSchema);
 module.exports = Visitor;
